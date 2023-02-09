630c4d3412163d92ad96abd3fc81c7406bb15d3a
Slendermen126 <83235367+Slendermen126@users.noreply.github.com>
2023-02-08 19:58:04 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        # # # # #
+        `)
+    basic.clearScreen()
+    basic.pause(200)
+}
+basic.forever(function () {
+	
+})
